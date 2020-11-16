705422ef4c93e34309a7e8cc576c55808cf46531
algorithm0x02 <algorithm0x02@gmail.com>
2020-11-17 02:08:31 +0900

    Home, Search and Account pages outline

---
+++ lib/root_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clon_by_som/tab_page.dart';
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TabPage();
+  }
+}
+
+ 
